cb6da7484259c8895be47a3871afe736b06d5e86
Mark Tamis <mark.tamis@gmail.com>
2022-12-13 19:24:53 +0100

    clean up of unused functions

--- src/hocs/index.tsx
+++ src/hocs/index.tsx
@@ -1,84 +1,6 @@
-import { ComponentInterface, FunctionalComponent, h } from '@stencil/core'
-import { getAssetPath, Host, HTMLStencilElement, JSXBase } from '@stencil/core/internal'
+import { h } from '@stencil/core'
+import { getAssetPath, Host } from '@stencil/core/internal'
 
-import { JSX } from '../components'
-
-/**
- * HOC to wrap components with Animatable component
- * @param WrappedComponent - Component to be animated with Animatable component
- */
-export function createAnimatableComponent<Props>(WrappedComponent: FunctionalComponent) {
-  type ComponentProps = Props &
-    ComponentInterface &
-    JSX.AnimateThis &
-    JSXBase.HTMLAttributes<HTMLElement> &
-    JSXBase.HTMLAttributes<HTMLStencilElement> &
-    JSXBase.HTMLAttributes<HTMLAnimateThisElement>
-
-  return (props: ComponentProps): FunctionalComponent<ComponentProps> => {
-    const {
-      ref,
-      animation,
-      keyFrames,
-      keyFramesData,
-      options,
-      optionsData,
-      animateId,
-      delay,
-      endDelay,
-      duration,
-      direction,
-      composite,
-      easing,
-      fill,
-      iterations,
-      iterationStart,
-      iterationComposite,
-      autoPlay,
-      fromClassName,
-      toClassName,
-      currentTime,
-      startTime,
-      playbackRate,
-      onRun,
-      onStop,
-      onCancel,
-      ...rest
-    } = props
-    return (
-      <animate-this
-        ref={ref}
-        animation={animation}
-        keyFrames={keyFrames}
-        keyFramesData={keyFramesData}
-        options={options}
-        optionsData={optionsData}
-        animateId={animateId}
-        delay={delay}
-        endDelay={endDelay}
-        duration={duration}
-        direction={direction}
-        composite={composite}
-        easing={easing}
-        fill={fill}
-        iterations={iterations}
-        iterationStart={iterationStart}
-        iterationComposite={iterationComposite}
-        autoPlay={autoPlay}
-        fromClassName={fromClassName}
-        toClassName={toClassName}
-        currentTime={currentTime}
-        startTime={startTime}
-        playbackRate={playbackRate}
-        onRun={onRun}
-        onStop={onStop}
-        onCancel={onCancel}
-      >
-        <WrappedComponent {...rest} />
-      </animate-this>
-    )
-  }
-}
 /**
  * Wrapper to style components with Tailwind
  * @source
@@ -86,10 +8,10 @@ export function createAnimatableComponent<Props>(WrappedComponent: FunctionalCom
  * @param children
  * @returns VNode | VNode[]
  */
-export const TailwindCss: typeof Host = (attrs, children) => {
+export const TailwindHost: typeof Host = (attrs, children) => {
   return (
     <Host {...attrs}>
-      <link rel="stylesheet" href={getAssetPath('buzzcasting-common.css')} />
+      <link rel="stylesheet" href={getAssetPath('components-base.css')} />
       {children}
     </Host>
   )
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-export * from './animate'
 export * from './dom'
 export * from './idb'
 export * from './messages'
 export * from './utils'
-export * from './waapi'
